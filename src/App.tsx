190ba45edcb25f3e456dff37dcf86369ab74ee68
jun <jun@Ubt2200.aaaaa>
2023-12-17 13:46:54 +0900

    とりあえずv1.0.0

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
 import React from 'react';
 import './App.css';
+import { Canvas } from '@react-three/fiber'
 
 function App() {
   return (
-    <div className="App">
-      hello world!!
+    <div id="canvas-container">
+      <Canvas>
+        <ambientLight />
+        <directionalLight />
+        <mesh>
+          <boxGeometry />
+          <meshStandardMaterial />
+        </mesh>
+      </Canvas>
     </div>
   );
 }
